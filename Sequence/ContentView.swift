dda47a65cb6cda09b0d9ff4cb61b7d98a15fb3e6
Vitaliy Voronok <vitalii.voronok@skylum.com>
2020-04-29 00:33:19 +0300

    Initial Commit

---
+++ Sequence/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sequence
+//
+//  Created by Vitaliy Voronok on 29.04.2020.
+//  Copyright © 2020 Vitaliy Voronok. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
